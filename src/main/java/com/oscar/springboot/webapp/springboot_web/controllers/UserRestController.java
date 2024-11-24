1e81bf8af6336de4c61616074f89b993ee333fba
Oscar Contreras <osccontrer.s@gmail.com>
2024-11-24 11:01:39 -0600

    Rest y models

--- src/main/java/com/oscar/springboot/webapp/springboot_web/controllers/UserRestController.java
+++ src/main/java/com/oscar/springboot/webapp/springboot_web/controllers/UserRestController.java
@@ -6,21 +6,26 @@ import java.util.Map;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.oscar.springboot.webapp.springboot_web.models.User;
+
 @RestController
+@RequestMapping("/api")
 
 public class UserRestController {
 
-    @GetMapping("/details2")
+    @GetMapping(path="/details")
     public Map<String, Object> details() {
-        
+
+        User user = new User("Oscar", "Contreras");
+
         Map<String, Object> body = new HashMap<>();
 
         body.put("title", "Hola mundo Spring Boot");
-        body.put("name", "Oscar");
-        body.put("lastname", "Contreras");
-
+        body.put("User", user);
+        
         return body;
     }
      
